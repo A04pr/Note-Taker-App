01d8e56ff9ef9cf0e2315fac9eb06ba9c0d06a8e
A04pr <areed041704@gmail.com>
2024-02-10 12:39:48 -0700

    restarting

--- writeNotesToFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-function writeNotesToFile(notes) {
-  const filePath = path.join(__dirname, 'db', 'db.json');
-  fs.writeFileSync(filePath, JSON.stringify(notes, null, 2));
-}
-
-module.exports = { writeNotesToFile };
